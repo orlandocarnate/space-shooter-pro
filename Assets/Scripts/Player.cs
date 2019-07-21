23eac93cd55bc1442924b9abaf9292c83825c385
Orlando Carnate <orlando.carnate@gmail.com>
2019-07-21 16:58:52 -0500

    add inputs using transform.Translate and Input.GetAxis

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // public or private reference
+    // data type (int, float, bool, string)
+    // every variable has a name
+    // optional: assigned value
+
+    [SerializeField]
+    private float speed = 3.5f; // default is 0 if unassigned. 'f' is required for floats
+
+    public float horizInput, verticalInput;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +26,19 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
         // 1 unit is 1 meter ; new Vector3(1,0,0) * RealTime(deltaTime) * 5
         // use Time.deltaTime to run 1m/sec
-        transform.Translate(Vector3.right * Time.deltaTime * 5);
+        // transform.Translate(Vector3.right * Time.deltaTime * speed * horizInput);
+        // transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
+
+        // transform.Translate(new Vector3(horizInput, verticalInput, 0) * speed * Time.deltaTime);
+
+        // best practice version:
+        Vector3 direction = new Vector3(horizInput, verticalInput, 0);
+        transform.Translate(direction * speed * Time.deltaTime);
+
     }
 }
