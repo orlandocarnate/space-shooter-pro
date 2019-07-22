a86d865f21d671fd5f8fc2391ed954716de5ffd2
Orlando Carnate <orlando.carnate@gmail.com>
2019-07-21 21:53:46 -0500

    Start Enemy prefab and scripts.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(Random.Range(-11f, 11f), 7, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move down 4m/s
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        // if exits bottom of screen, respawn at top with new random x position
+        if (transform.position.y < -7)
+        {
+            float randomX = Random.Range(-11f, 11f);
+            transform.position = new Vector3(randomX, 7, 0);
+        } 
+
+    }
+
+}
